9853a43ebfc9bf280c4969f7e4a42ca9bb2774c5
labani26 <mail2labanisardar@gmail.com>
2025-03-01 13:14:28 +0530

    added profile page

---
+++ src/pages/ProductsPage.js
@@ -0,0 +1,13 @@
+// src/pages/ProductsPage.js
+import React from 'react';
+
+const ProductsPage = () => {
+  return (
+    <div>
+      <h1>Our Products</h1>
+      <p>Explore our amazing range of products.</p>
+    </div>
+  );
+};
+
+export default ProductsPage;
